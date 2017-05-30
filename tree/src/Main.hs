3cb01323c1c0ea0e7766c64d8ec34b71b4139cf3
Roman <tkachukroman64@gmail.com>
2017-05-30 18:53:20 +0300

    Making code more readable

--- tree/src/Main.hs
+++ tree/src/Main.hs
@@ -2,9 +2,17 @@ generateTreeString :: Int -> Int -> String
 generateTreeString starAmount spaceAmount =
   do
     if spaceAmount <= 0
-      then replicate spaceAmount ' ' ++ replicate starAmount '*' ++ "\n" ++ replicate ((starAmount-1) `div` 2) ' ' ++ "*\n"
-    else
-      do replicate spaceAmount ' ' ++ replicate starAmount '*' ++ "\n" ++ generateTreeString (starAmount+2) (spaceAmount-1)
+      then 
+        replicate spaceAmount ' ' ++ 
+        replicate starAmount '*' ++ 
+        "\n" ++ 
+        replicate ((starAmount-1) `div` 2) ' ' ++ 
+        "*\n"
+    else 
+      replicate spaceAmount ' ' ++ 
+      replicate starAmount '*' ++ 
+      "\n" ++ 
+      generateTreeString (starAmount+2) (spaceAmount-1)
 
 printTree :: Int -> String
 printTree treeSize = generateTreeString 1 ((treeSize-1) `div` 2)
