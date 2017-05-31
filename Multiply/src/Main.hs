9a65f6f2b7d7e876c6eb58685fb3c2f6f666dc8f
Roman <tkachukroman64@gmail.com>
2017-05-31 18:41:37 +0300

    Reworking Multiply project as buisness-logic program

--- Multiply/src/Main.hs
+++ Multiply/src/Main.hs
@@ -4,7 +4,15 @@ recursiveUserInputProcess listOfValues = do
   userInput <- getLine
   let valueToAdd = (read userInput :: Double)
   case valueToAdd of
-    0 -> print $ product listOfValues
-    _ -> recursiveUserInputProcess (valueToAdd : listOfValues)
+    0 -> showResult listOfValues
+    _ -> recursiveUserInputProcess (valueToAdd `addValue` listOfValues)
+
+addValue :: Double -> [Double] -> [Double]
+addValue value valueList =
+  value : valueList
+
+showResult :: [Double] -> IO()
+showResult listOfValues =
+  print $ product listOfValues
 
 main = recursiveUserInputProcess []
