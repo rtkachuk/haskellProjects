8d8432f8b3e7f2fc017f540b396aed823b3f316a
Roman <tkachukroman64@gmail.com>
2017-05-31 19:11:06 +0300

    Fixed buisness-logic in Multiply project

--- Multiply/src/Main.hs
+++ Multiply/src/Main.hs
@@ -1,8 +1,12 @@
+getValue :: IO Double
+getValue = do
+  userInput <- getLine
+  return (read userInput :: Double)
+
 recursiveUserInputProcess :: [Double] -> IO()
 recursiveUserInputProcess listOfValues = do
   putStrLn ("Enter value: ")
-  userInput <- getLine
-  let valueToAdd = (read userInput :: Double)
+  valueToAdd <- getValue
   case valueToAdd of
     0 -> showResult listOfValues
     _ -> recursiveUserInputProcess (valueToAdd `addValue` listOfValues)
