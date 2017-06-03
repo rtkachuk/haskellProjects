be9845f0fd077179923d18abb3c7cdeaee7151a5
Tkachuk Roman <tkachukroman64@gmail.com>
2017-06-03 17:55:55 +0300

    Commented Calculator project

--- Calculator/src/Main.hs
+++ Calculator/src/Main.hs
@@ -1,15 +1,23 @@
+-- Functions without declaration
+
 calcAdd a b = a + b
 calcSub a b = a - b
 calcMul a b = a * b
 calcDiv a b = a / b
 
 main = do
+
+-- Here we will read action, first and second argument from user input
+
   putStrLn("Give me first value: ")
   first <- getLine
   putStrLn("Give me second value: ")
   second <- getLine
   putStrLn("?Action: ")
   func <- getChar
+
+-- Transform it to double, and with if select action
+
   let x = (read first :: Double)
   let y = (read second :: Double)
   if func == '1'
