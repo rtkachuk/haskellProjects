f2ec6974503e9c5c893148b8fa09dda8fda7aaac
Tkachuk Roman <tkachukroman64@gmail.com>
2017-06-03 17:58:09 +0300

    Commented CalculatorCase project

--- CalculatorCase/src/Main.hs
+++ CalculatorCase/src/Main.hs
@@ -1,15 +1,23 @@
+-- Functions without declaration
+
 calcAdd a b = a + b
 calcSub a b = a - b
 calcMul a b = a * b
 calcDiv a b = a / b
 
 main = do
+
+-- Here we will read action, first and second argument from user input
+
   putStrLn("Give me first value: ")
   first <- getLine
   putStrLn("Give me second value: ")
   second <- getLine
   putStrLn("?Action: ")
   func <- getChar
+
+-- Transform arguments to double, and select action with using case
+
   let x = (read first :: Double)
   let y = (read second :: Double)
   case func of
