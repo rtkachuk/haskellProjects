4bca21d7654ad609bb55c7e390877debf197f1ff
Roman <tkachukroman64@gmail.com>
2017-07-14 18:58:33 +0300

    Updated polindrom project

--- polindrom/src/Main.hs
+++ polindrom/src/Main.hs
@@ -1,18 +1,10 @@
 {-# LANGUAGE MultiWayIf #-}
+
 import System.Environment
 import Data.Char
 
-checkPolindrome :: String -> Bool
-checkPolindrome str = do
-   if | (length str > 1) -> do
-                            if (head str) == (last str) then checkPolindrome (tail $ init $ str)
-                            else False
-      | otherwise -> True
-     
-
 main = do
   args <- getArgs
-  if checkPolindrome (map (\n -> toLower n) $ filter isLetter $ head args) == True then
-    putStrLn ("OK!")
-  else
-    putStrLn ("NO!")
+  let word = map toLower $ filter isLetter $ head args
+  if | word == reverse word -> putStrLn $ "OK!"
+     | otherwise -> putStrLn $ "NO!"
