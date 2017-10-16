f82b410e2b23b3acc3bdd920f6ceb4dc9ff884fe
Tkachuk Roman <tkachukroman64@gmail.com>
2017-10-16 12:39:30 +0300

    Fixed concurrentServer project! now it works as expected!

--- concurrentServer/src/Main.hs
+++ concurrentServer/src/Main.hs
@@ -1,35 +1,47 @@
 {-# LANGUAGE MultiWayIf #-}
 
 module Main where
-
 import Network
 import Control.Concurrent
 import System.IO
-import System.Exit
-import System.Posix.Process.ByteString
+import Data.List
+
+import Control.Monad
 
 logFile = "/home/fabler/log.txt"
 
 main :: IO ()
 main = withSocketsDo $ do
        f <- openFile logFile AppendMode
-       hSetBuffering f NoBuffering
+       hSetBuffering f LineBuffering
+       mVar <- newEmptyMVar
+       putMVar mVar f
        socket <- listenOn $ PortNumber 5002
-       loop socket f
+       forever $ loop socket mVar
 
-loop sock f = do
+loop sock m = do
   (h,hn,pn) <- accept sock
-  hSetBuffering h NoBuffering
-  forkIO $ workWithSocket h f
-  loop sock f
+  hSetBuffering h LineBuffering
+  forkIO $ workWithSocket h m
 
-workWithSocket h f = do
-  message <- hGetContents $ h
-  let check = filter (/= '\n') message 
-  if | check == "quit" -> do
-                            hClose f
-                            hClose h
-                            exitImmediately ExitSuccess
-     | otherwise -> do
-                    hPutStr f message
-  hClose h
+workWithSocket h m = do
+  message <- hGetLine $ h
+
+  let var = "G" `isInfixOf` message
+--  if | "G" `isInfixOf` message -> workWithMVar m "GET REQUEST"
+--     | otherwise -> workWithMVar m message
+
+  workWithMVar m message
+  workWithMVar m $ show var
+  check <- hIsEOF h
+
+  if check then
+    hClose h
+  else
+    workWithSocket h m
+
+workWithMVar m str = do
+  f <- takeMVar m
+  hPutStr f str
+  hFlush f
+  putMVar m f
