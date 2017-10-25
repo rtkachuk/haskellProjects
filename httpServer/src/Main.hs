a6a449fe5d039b99742c5f1d892ab33b2c9814bf
Tkachuk Roman <tkachukroman64@gmail.com>
2017-10-25 15:06:53 +0300

    Implemented fully-functional server

--- httpServer/src/Main.hs
+++ httpServer/src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiWayIf #-}
+ {-# LANGUAGE MultiWayIf #-}
 
 module Main where
 
@@ -8,43 +8,127 @@ import Network.HTTP.Server.HtmlForm as Form
 import Network.URL as URL
 import Codec.Binary.UTF8.String
 import Control.Exception(try, SomeException)
+import Control.Concurrent
+import Control.Concurrent.Chan
+import Control.Monad (forever)
 import System.FilePath
+import System.IO
 import Text.XHtml
 import Text.JSON
 import Text.JSON.String(runGetJSON)
 import Data.List
+import Control.Exception (evaluate)
 
 import Parsers
 
 dbFile = "/home/fabler/db.txt"
 tempFile = "/tmp/HaskellHttpServerTempData.txt"
 
+data ChannelMode = CGet | CPostRead | CPostWrite | CDelete
+
 main :: IO ()
-main = serverWith defaultConfig { srvLog = stdLogger, srvPort=8888 } $ \_ url request -> do
+main = do
+  actionChannel <- newChan
+  getChannel <- newChan
+  postReadChannel <- newChan
 
-  dataFromFile <- readFile dbFile
-  
-  let req = decodeString $ rqBody request
-      ur = url_path url
-  case rqMethod request of
+  worker $ writerListener actionChannel getChannel postReadChannel
+  serverWith defaultConfig { srvLog = stdLogger, srvPort=8888 } $ \_ url request -> do
 
-    GET -> readRequest ur dataFromFile
+    let req = decodeString $ rqBody request
+        ur = url_path url
+    case rqMethod request of
 
-    POST -> processPost ur req dataFromFile
+      GET -> do getRequest ur actionChannel getChannel
+
+      POST -> processPost ur req actionChannel postReadChannel
     
-    DELETE -> processDelete ur dataFromFile
+      DELETE -> processDelete ur actionChannel
 
-    _ -> return $ prepareHtml BadRequest $ toHtml "Not implemented"
+      _ -> return $ prepareHtml BadRequest $ toHtml "Not implemented"
 
-readRequest url dataFromFile = do
-  let text = findData $ seekData (lines dataFromFile) $ url
+--
+-- Worker. It actually starts forever threads for reading/writing
+--
 
-  if | length text == 0 -> sendJSONNoData
-     | otherwise -> sendJSON text
+worker action = forkIO $ forever action
 
-writeRequest ur req dataFromFile = do
-  ans <- tryWrite ur req dataFromFile
-  return $ ans
+--
+-- Chanel, which will have acsess to file
+--
+
+writerListener actionChannel getChannel postReadChannel= do
+  dat <- readChan actionChannel
+  processChannelRecv dat getChannel postReadChannel
+
+processChannelRecv (mode, dat) getChannel postReadChannel =
+  case mode of
+    CGet -> do
+      dataFromFile <- getDataFromFile dbFile
+      let text = findData $ seekData (lines dataFromFile) $ dat
+      writeChan getChannel text
+    CPostRead -> do
+      dataFromFile <- getDataFromFile dbFile
+      let text = findData $ seekData (lines dataFromFile) $ dat
+      writeChan postReadChannel text
+    CPostWrite -> do
+      txt <- getDataFromFile dbFile
+      let key = findKey dat
+      let str = findData dat
+      writeDataToFile tempFile txt
+      buff <- getDataFromFile tempFile
+      writeDataToFile dbFile $ unlines $ filter (\str -> (key ++ ":") `isInfixOf` str == False) $ lines buff
+      appendDataToFile dbFile $ key ++ ":" ++ str ++ "\n"
+    CDelete -> do
+      txt <- getDataFromFile dbFile
+      let key = dat
+      writeDataToFile tempFile txt
+      buff <- getDataFromFile tempFile
+      writeDataToFile dbFile $ unlines $ filter (\str -> (key ++ ":") `isInfixOf` str == False) $ lines buff
+
+getDataFromFile path = do
+  file <- openFile path ReadMode
+  text <- hGetContents file
+  evaluate (length text)
+  hClose file
+  return text
+
+writeDataToFile path dat = do
+  file <- openFile path WriteMode
+  hPutStr file dat
+  hFlush file
+  hClose file
+
+appendDataToFile path dat = do
+  file <- openFile path AppendMode
+  hPutStr file dat
+  hFlush file
+  hClose file
+
+--
+--  Read (GET) processing
+--
+
+getRequest url actionChannel readChannel = do
+
+  writeChan actionChannel (CGet, url)
+  text <- readChan readChannel
+
+  sendReadResult text
+
+postReadRequest url actionChannel readChannel = do
+  writeChan actionChannel (CPostRead, url)
+  text <- readChan readChannel
+
+  sendReadResult text
+
+sendReadResult text =
+  if | length text == 0 -> do
+        ans <- sendJSONNoData
+        return ans
+     | otherwise -> do
+        ans <- sendJSON text
+        return ans
 
 generateJSONObject status text = do
   let dataObject = ("data", JSString $ toJSString $ text)
@@ -60,6 +144,15 @@ sendJSONNoData = do
   obj <- generateJSONObject "FAIL" "<no data>"
   return $ prepareJSON NotFound $ obj
 
+
+--
+-- Write processing
+--
+
+writeRequest ur req dataFromFile = do
+  ans <- tryWrite ur req dataFromFile
+  return $ ans
+
 sendText s v = insertHeader HdrContentLength (show (length v)) $
                insertHeader HdrContentEncoding "UTF-8" $
                insertHeader HdrContentEncoding "text/plain" $
@@ -72,28 +165,20 @@ prepareHtml s v = insertHeader HdrContentType "text/html" $ sendText s $ renderH
 prepareJSON :: StatusCode -> JSValue -> Response String
 prepareJSON s v = insertHeader HdrContentType "application/json" $ sendText s $ showJSValue v ""
 
-processPost ur req dataFromFile = do
-  if | length req == 0 -> readRequest ur dataFromFile
-     | otherwise -> writeRequest ur req dataFromFile
+processPost ur req actionChannel postReadChannel = do
+  if | length req == 0 -> postReadRequest ur actionChannel postReadChannel
+     | otherwise -> writeRequest ur req actionChannel
 
-processDelete ur dataFromFile = do
-  writeFile tempFile dataFromFile
-  buff <- readFile tempFile
-  writeFile dbFile $ unlines $ filter (\str -> (ur ++ ":") `isInfixOf` str == False) $ lines buff
+processDelete ur actionChannel = do
+  writeChan actionChannel (CDelete, ur)
   ans <- sendJSON "REMOVED"
   return $ ans
 
-tryWrite key dat txt = do
-  if (keyIsValid key) then
-    writeToFile key dat txt
+tryWrite key dat actionChannel = do
+  if (keyIsValid key) then do
+    writeChan actionChannel (CPostWrite, (key ++ ":" ++ dat))
+    ans <- sendJSON "OK"
+    return $ ans
   else do
     ans <- sendJSONNoData
     return $ ans
-
-writeToFile key dat txt = do
-  writeFile tempFile txt
-  buff <- readFile tempFile
-  writeFile dbFile $ unlines $ filter (\str -> (key ++ ":") `isInfixOf` str == False) $ lines buff
-  appendFile dbFile $ key ++ ":" ++ dat ++ "\n"
-  ans <- sendJSON dat
-  return $ ans
